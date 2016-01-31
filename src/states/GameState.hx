78441e694d0ecb330a4d35c0ba3abe584a434147
Javier Sivack <sivackjavier@gmail.com>
2016-01-31 17:00:23 -0300

    Andan los enemigos.

--- src/states/GameState.hx
+++ src/states/GameState.hx
@@ -99,7 +99,7 @@ class GameState extends FlxState
 		FlxG.collide(map, player);
 		FlxG.collide(hackWall, player);
 		player.updateRitualObjectPosition();
-		//EnemyManager.instance.enemyUpdates(map, player);
+		EnemyManager.instance.enemyUpdates(map, player);
 		PentagramManager.instance.pentagramUpdate(player);
 		FlxG.collide(player, caldero);
 		checkGameOver();
